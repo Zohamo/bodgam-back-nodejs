7a5a7b8f66c7564cd13025a3e260912af4e39d75
Zohamo <thiazsch@gmail.com>
2019-11-07 13:00:30 -0600

    :wrench: set local development settings

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ console.log("API server started on: " + port);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// JUST TO TEST LOCALLY : removes CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Routes
 var routes = require("./api/routes");
 routes(app); // register the route
